0c66602b4029b689a189a63b0f00941d973b9e24
Ambitioner-c <656359504@qq.com>
2019-12-10 20:15:25 +0800

    去重

---
+++ py/de_repetition.py
@@ -0,0 +1,32 @@
+import csv
+
+
+# 把问文件去重，并重写到2中
+# doctor_href_set = set([])
+#
+# doctor_home_detail = csv.reader(open('../data/doctor_home_detail_finish.csv', 'r'))
+# for i in doctor_home_detail:
+#     if i[0] not in doctor_href_set:
+#         doctor_href_set.add(i[0])
+#         with open('../data/doctor_home_detail_finish2.csv', 'a') as f:
+#             writer = csv.writer(f)
+#             writer.writerow(i)
+
+
+# 判断文件是否存在重复
+doctor_home_detail = csv.reader(open('../data/doctor_home_detail_finish2.csv', 'r'))
+
+doctor_href_list = []
+for i in doctor_home_detail:
+    doctor_href_list.append(i[0])
+doctor_href_set = set(doctor_href_list)
+print(len(doctor_href_list))
+print(len(doctor_href_set))
+
+
+# 检测是否存在逗号
+# doctor_home_detail = csv.reader(open('/home/cfl/Downloads/qq-files/656359504/file_recv/doctor_home_detail3.csv', 'r'))
+# for i in doctor_home_detail:
+#     length = len(i)
+#     if length != 27:
+#         print(i[0])

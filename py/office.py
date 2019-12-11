6acfd38e14a56baafcf1e3a3158fe4bf8f6b6e5a
Ambitioner-c <656359504@qq.com>
2019-12-11 22:28:46 +0800

    获取科室列表

--- py/office.py
+++ py/office.py
@@ -1,6 +1,6 @@
 """
 author: cfl
-    获取科室列表。
+    获取科室列表
 """
 import requests
 from bs4 import BeautifulSoup
@@ -163,7 +163,7 @@ class MyThread(threading.Thread):
                 write_finish(self.pathname, j)
 
                 finish = finish + 1
-                print('线程(%s):已完成%s个医院!' % (thread_name, finish))
+                print('线程(%s):已完成%s个office!' % (thread_name, finish))
             except:
                 os.system('spd-say "error"')
                 print('\033[5;30;47m【错误：%s】\033[0m获取数据出现异常！' % j)
@@ -172,7 +172,7 @@ class MyThread(threading.Thread):
                 write_error(self.pathname, j)
 
                 error = error + 1
-                print('线程(%s):已错误%s个医院!' % (thread_name, error))
+                print('线程(%s):已错误%s个office!' % (thread_name, error))
 
                 continue
             end = time.time()

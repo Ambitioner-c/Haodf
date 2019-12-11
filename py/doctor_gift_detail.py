6642b537695ac85344e4f31228e625de0a4300ba
Ambitioner-c <656359504@qq.com>
2019-12-11 22:25:07 +0800

    获取医生的礼物信息

--- py/doctor_gift_detail.py
+++ py/doctor_gift_detail.py
@@ -3,7 +3,7 @@
 # time   : 2019/12/11 下午20:45
 """
 author: cfl
-    获取医生的礼物信息。
+    获取医生的礼物信息
 """
 from bs4 import BeautifulSoup
 import re
@@ -198,16 +198,16 @@ class MyThread(threading.Thread):
                 write_finish(self.pathname, self.doctor_href_list[j])
 
                 finish = finish + 1
-                print('线程(%s):已完成%s个医生!' % (thread_name, finish))
+                print('线程(%s):已完成%s个gift!' % (thread_name, finish))
             except:
                 os.system('spd-say "error"')
-                print('\033[5;30;47m【错误：%s】\033[0m获取数据出现异常！' % j)
+                print('\033[5;30;47m【错误：%s】\033[0m获取数据出现异常！' % self.doctor_href_list[j])
 
                 # 写入错误
                 write_error(self.pathname, self.doctor_href_list[j])
 
                 error = error + 1
-                print('线程(%s):已错误%s个医生!' % (thread_name, error))
+                print('线程(%s):已错误%s个gift!' % (thread_name, error))
 
                 continue
             end = time.time()

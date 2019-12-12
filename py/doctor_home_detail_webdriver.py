7f679c06a820d3c6d62961c78825a0504a70235e
Ambitioner-c <656359504@qq.com>
2019-12-12 16:50:02 +0800

    获取医生主页信息（webdriver）

--- py/doctor_home_detail_webdriver.py
+++ py/doctor_home_detail_webdriver.py
@@ -1,3 +1,6 @@
+# _*_ coding : UTF-8 _*_
+# author : cfl
+# time   : 2019/12/12 下午16:49
 """
 author: cfl
     获取医生主页信息，成功，
@@ -21,6 +24,11 @@ Headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
 
 
 def get_driver(executable_path):
+    """
+    获取webdriver
+    :param executable_path:
+    :return: driver
+    """
     chrome_opt = Options()  # 创建参数设置对象.
     chrome_opt.add_argument('--headless')  # 无界面化.
     chrome_opt.add_argument('--disable-gpu')  # 配合上面的无界面化.
@@ -36,6 +44,14 @@ def get_driver(executable_path):
 
 
 def get_doctor_home(driver, pathname, doctor_href, doctor_home):
+    """
+    获取医生主页信息，并写入文件
+    :param driver:
+    :param pathname:
+    :param doctor_href:
+    :param doctor_home:
+    :return:
+    """
     url = 'https://%s.haodf.com/' % doctor_home
 
     # 获取页面内容
@@ -78,141 +94,142 @@ def get_doctor_home(driver, pathname, doctor_href, doctor_home):
 
     #
     # 上方模块
-    div_up = html.findAll('div', attrs={'class': 'space_b_picright'})[0]
+    div_up = html.findAll('div', attrs={'class': re.compile('doctor-intro')})[0]
 
     # 职称
-    h3_title = div_up.findAll('h3', attrs={'class': re.compile(r'doc_name')})[0]
-    title = re.findall(r'>(.+?)</h3>', str(h3_title))[0]
-    title = title.replace(' ', '|').strip('|').replace('  ', '||')
+    h1_name = div_up.findAll('h1', attrs={'class': 'doctor-name'})[0]
+    name = re.findall(r'>(.+?)<', str(h1_name))[0]
+    span_title = div_up.findAll('span', attrs={'class': 'positon'})[0]
+    title_list = re.findall(r'>(.+?)</span>', str(span_title))
+    try:
+        title = name + '||' + title_list[0] + '|' + title_list[1]
+    except:
+        title = name + '||' + title_list[0]
 
     # 科室
-    div_office = div_up.findAll('div', attrs={'class': re.compile(r'doc_hospital')})[0]
-    p_office_list = div_office.findAll('div')[0].findAll('p')
+    p_office_list = div_up.findAll('p', attrs={'class': re.compile(r'doctor-faculty')})
     office = ''
     for j in p_office_list:
         office = office + re.findall(r'>(.+?)</a>', str(j))[0] + '|' + re.findall(r'>(.+?)</a>', str(j))[1] + '||'
     office = office.strip('|')
 
-    #
-    # 左边模块
-    # 有可能是停诊公告
-    j = 0
-    while 'doc_info' not in str(html.findAll('div', attrs={'class': 'mr_line1'})[j]):
-        j = j + 1
-    div_left = html.findAll('div', attrs={'class': 'mr_line1'})[j]
-
-    # 诊后服务星和推荐热度
-    ul_star_recommend = div_left.findAll('ul', attrs={'class': 'doc_info_ul1'})[0]
-    # 诊后服务星
-    try:
-        li_star = ul_star_recommend.findAll('li', attrs={'class': 'first_li'})[0]
-        star = str(5 - len(re.findall(r'zero', str(li_star))))
-    except:
-        star = ''
     # 推荐热度
-    li_recommend = ul_star_recommend.findAll('li', attrs={'': ''})[-2]
+    div_recommend = div_up.findAll('div', attrs={'class': re.compile(r'profile-sta')})[0]
+    p_recommend = div_recommend.findAll('p')[0]
     try:
-        i_recommend = li_recommend.findAll('i', attrs={'class': 'bigredinfo'})[-1]
+        i_recommend = p_recommend.findAll('i')[0]
         recommend = re.findall(r'>(.+?)</i>', str(i_recommend))[0]
     except:
         recommend = '推荐数据核实中'
 
-    # 擅长、简介
-    url_intro = url + '/api/index/ajaxdoctorintro?uname=%s' % doctor_home
-    # 获取页面内容
-    driver.get(url_intro)
-    html_intro = driver.page_source
-    html_intro = BeautifulSoup(html_intro, 'lxml')
+    #
+    # 中间模块
+    div_center = html.findAll('div', attrs={'class': re.compile('good-at-con')})[0]
+
+    # 擅长
     try:
-        # 擅长
-        p_skill = html_intro.findAll('p', attrs={'class': 'hh'})[0]
-        skill = ('' + p_skill.text).replace(' ', '').replace('"', '').replace('\\n', '').replace(',', '，')
+        div_skill = div_center.findAll('div', attrs={'class': 'good-at-text'})[0]
+        skill = str(re.findall(r'>(.+?)</div>', str(div_skill))[0]).replace(',', '，')
     except:
         skill = ''
+
+    # 简介
     try:
-        # 简介
-        p_intro = html_intro.findAll('p', attrs={'class': 'hh'})[1]
-        introduction = ('' + p_intro.text).replace(' ', '').replace('"', '').replace('\\n', '').replace(',', '，')
+        div_intro = div_center.findAll('div', attrs={'class': 'good-at-text'})[1]
+        introduction = div_intro.text.replace(',', '，')
     except:
         introduction = ''
 
-    # 医生id
-    a_doctor_id = div_left.findAll('a', attrs={'id': 'thankLetter'})[0]
-    doctor_id = re.findall(r'doctor_id=(.+?)&', str(a_doctor_id))[0]
+    #
+    # 在线、预约、团队、私人
+    online_appointment_team_private = []
+    div_online_appointment_team_private = html.findAll('div', attrs={'class': re.compile('entry-con')})[0]
+    a_online_appointment_team_private_list = div_online_appointment_team_private.findAll('a')
+    for j in a_online_appointment_team_private_list:
+        if 'enable' in str(j):
+            online_appointment_team_private.append('1')
+        else:
+            online_appointment_team_private.append('0')
+    # 在线
+    online = online_appointment_team_private[0]
+
+    # 预约
+    appointment = online_appointment_team_private[2]
+
+    # 团队
+    team = online_appointment_team_private[1]
+
+    # 私人
+    private = online_appointment_team_private[3]
 
     #
-    # 中间模块
-    div_center = html.findAll('div', attrs={'class': 'left_content'})[0]
-
     # 开场白
     try:
-        span_abstract = div_center.findAll('span', attrs={'id': 'span_announce_title'})[0]
-        p_abstract = div_center.findAll('p', attrs={'id': 'p_announce'})[0]
-        abstract = span_abstract.text + '|' + p_abstract.text
-        abstract = abstract.replace(' ', '').replace('\n', '').replace(',', '，')
+        div_abstract = html.findAll('div', attrs={'class': re.compile('welcome-con')})[0]
+        # 标题
+        div_abstract1 = div_abstract.findAll('div', attrs={'class': re.compile('item-welcome-title')})[0]
+        div_abstract1 = str(div_abstract1).replace('\n', '').replace(' ', '')
+        abstract1 = re.findall(r'el">(.+?)<', div_abstract1)[0].replace(',', '，')
+
+        # 内容
+        div_abstract2 = div_abstract.findAll('div', attrs={'class': re.compile('main-item-welcome')})[0]
+        div_abstract2 = str(div_abstract2).replace('\n', '').replace(' ', '')
+        abstract2 = re.findall(r'>(.+?)<', div_abstract2)[0].replace(',', '，')
+        abstract = abstract1 + '|' + abstract2
     except:
         abstract = ''
 
-    # 在线、预约、团队、私人
-    div_online_appointment_team_private = div_center.findAll('div', attrs={'class': 'd-s-items'})[0]
-    # 在线
-    try:
-        online = str(len(re.findall(r'zixun', str(div_online_appointment_team_private))))
-    except:
-        online = '0'
-    # 预约
-    try:
-        appointment = str(len(re.findall(r'guahao', str(div_online_appointment_team_private))))
-    except:
-        appointment = '0'
-    # 团队
-    try:
-        team = str(len(re.findall(r'teamdoc', str(div_online_appointment_team_private))))
-    except:
-        team = '0'
-    # 私人
-    try:
-        private = str(len(re.findall(r'familydoc', str(div_online_appointment_team_private))))
-    except:
-        private = '0'
+    #
+    # 医生id
+    div_doctor_id = html.findAll('div', attrs={'class': re.compile('patient-vote')})[0]
+    doctor_id = re.findall(r'doctor_id=(.+?)&', str(div_doctor_id))[0]
 
+    #
     # 患友会小组数
-    div_group = html.findAll('div', attrs={'class': 'mr_line1'})[-2]
     try:
-        span_group = div_group.findAll('span', attrs={'class': 'orange1'})[0]
-        group = span_group.text
+        div_group = html.findAll('div', attrs={'class': re.compile('patients-collect')})[0]
+        group = re.findall(r'>(\d+)</span>个患者讨论小组', str(div_group))[0]
     except:
         group = '0'
 
     #
-    # 下面模块
-    ul_down = html.findAll('ul', attrs={'class': 'space_statistics'})[0]
-    li_down_list = ul_down.findAll('li')
+    # 诊后服务星
+    try:
+        div_star = html.findAll('div', attrs={'class': re.compile('doc-experience')})[0]
+        span_star = div_star.findAll('span', attrs={'class': re.compile('experience-data')})[0]
+        star = str(len(re.findall(r'star_yellow', str(span_star))))
+    except:
+        star = ''
+
+    #
+    # 最重要模块
+    div_inpor = html.findAll('div', attrs={'class': re.compile('person-web')})[0]
+    div_inpor_list = div_inpor.findAll('div', attrs={'class': 'per-sta-data'})
 
     li_list = []
-    for j in li_down_list:
-        li_list.append(str(re.findall(r'">(.+?)</span>', str(j))[0]).replace(',', ''))
+    for j in div_inpor_list:
+        li_list.append(str(re.findall(r'">(.+?)<', str(j))[0]).replace(',', ''))
 
     # 总访问
-    interviews = li_list[0]
+    interviews = li_list[0].replace('次', '')
     # 昨日访问
-    interview = li_list[1]
+    interview = re.findall(r'(\d+)次', str(li_list[1]))[0]
     # 总文章
-    article = li_list[2]
+    article = li_list[2].replace('篇', '')
     # 总患者
-    patients = li_list[3]
+    patients = li_list[3].replace('位', '')
     # 昨日诊后报到患者
-    patient = li_list[4]
+    patient = li_list[4].replace('位', '')
     # 微信诊后报到患者
-    wechat = li_list[5]
+    wechat = li_list[5].replace('位', '')
     # 总诊后报到患者
-    total = li_list[6]
+    total = li_list[6].replace('位', '')
     # 患者投票
-    vote = li_list[7]
+    vote = li_list[7].replace('票', '')
     # 感谢信
-    thanks = li_list[8]
+    thanks = li_list[8].replace('封', '')
     # 心意礼物
-    gift = li_list[9]
+    gift = li_list[9].replace('个', '')
     # 上次在线
     last = li_list[10]
     # 开通时间
@@ -229,6 +246,11 @@ def get_doctor_home(driver, pathname, doctor_href, doctor_home):
 
 
 def read_doc(pathname):
+    """
+    读取待爬列表
+    :param pathname:
+    :return: doctor_href_list, doctor_home_list
+    """
     # 全列表
     doctor_href_all_list = []
     doctor_home_all_list = []
@@ -264,7 +286,11 @@ def read_doc(pathname):
 
 
 def write_table(pathname):
-
+    """
+    写入表头
+    :param pathname:
+    :return:
+    """
     # 判断是否已经写入表头
     try:
         with open(pathname + 'doctor_home_detail.csv', 'r') as doctor_home_detail_reader:
@@ -311,6 +337,38 @@ def write_doc(pathname,
               abstract, online, team, appointment, private,
               group, interviews, interview, article, patients, patient, wechat, total, vote, thanks, gift, last, dredge,
               now):
+    """
+    写入文件
+    :param pathname:
+    :param doctor_href:
+    :param doctor_id:
+    :param title:
+    :param office:
+    :param star:
+    :param recommend:
+    :param skill:
+    :param introduction:
+    :param abstract:
+    :param online:
+    :param team:
+    :param appointment:
+    :param private:
+    :param group:
+    :param interviews:
+    :param interview:
+    :param article:
+    :param patients:
+    :param patient:
+    :param wechat:
+    :param total:
+    :param vote:
+    :param thanks:
+    :param gift:
+    :param last:
+    :param dredge:
+    :param now:
+    :return:
+    """
     # 医生主页
     with open(pathname + 'doctor_home_detail.csv', 'a') as doctor_home_detail:
         doctor_home_detail_writer = csv.writer(doctor_home_detail)
@@ -325,6 +383,12 @@ def write_doc(pathname,
 
 
 def write_finish(pathname, doctor_href):
+    """
+    写入成功
+    :param pathname:
+    :param doctor_href:
+    :return:
+    """
     with open(pathname + 'doctor_home_detail_finish.csv', 'a') as doctor_home_detail_finish:
         doctor_home_detail_finish_writer = csv.writer(doctor_home_detail_finish)
 
@@ -333,6 +397,12 @@ def write_finish(pathname, doctor_href):
 
 
 def write_error(pathname, doctor_href):
+    """
+    写入失败
+    :param pathname:
+    :param doctor_href:
+    :return:
+    """
     with open(pathname + 'doctor_home_detail_error.csv', 'a') as doctor_home_detail_error:
         doctor_home_detail_error_writer = csv.writer(doctor_home_detail_error)
 
@@ -399,7 +469,7 @@ if __name__ == '__main__':
     # my_doctor_home_list = my_doctor_home_list[2500:]
 
     # get_doctor_home(my_driver, my_pathname, my_doctor_href_list[0], my_doctor_home_list[0])
-    # get_doctor_home(my_driver, my_pathname, 'DE4r08xQdKSLf-2EkxEsNJ7qcZX0', 'qianqiuhai')
+    # get_doctor_home(my_driver, my_pathname, 'DE4r0BCkuHzdeZOlwlzfz6DLFjxaq', 'drzjxcn')
     # write_finish(my_pathname, 'DE4r08xQdKSLf-2EkxEsNJ7qcZX0')
 
     # 将医生分成3等份

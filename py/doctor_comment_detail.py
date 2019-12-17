8a666b05c0ea9ccfa75e54e7d93a6b67e6e5ae9c
Ambitioner-c <656359504@qq.com>
2019-12-17 17:03:06 +0800

    获取医生评论信息

--- py/doctor_comment_detail.py
+++ py/doctor_comment_detail.py
@@ -497,31 +497,31 @@ if __name__ == '__main__':
     my_doctor_href_list = read_doc(my_pathname)
 
     # get_doctor_comment_vote(my_pathname, my_doctor_href_list[1])
-    # get_doctor_comment_vote(my_pathname, 'DE4r0eJWGqZNwMGX-qrdcxIqpOOMCeP0')
-    # write_finish(my_pathname, 'DE4r0eJWGqZNwMGX-qrdcxIqpOOMCeP0')
+    get_doctor_comment_vote(my_pathname, 'DE4r0Fy0C9LuwlwRKORH3rVxD-uCU22Ut')
+    write_finish(my_pathname, 'DE4r0Fy0C9LuwlwRKORH3rVxD-uCU22Ut')
 
-    # 将医生分成5等份
-    my_doctor_href_list1 = my_doctor_href_list[int(len(my_doctor_href_list)/5)*0:int(len(my_doctor_href_list)/5)*1]
-    my_doctor_href_list2 = my_doctor_href_list[int(len(my_doctor_href_list)/5)*1:int(len(my_doctor_href_list)/5)*2]
-    my_doctor_href_list3 = my_doctor_href_list[int(len(my_doctor_href_list)/5)*2:int(len(my_doctor_href_list)/5)*3]
-    my_doctor_href_list4 = my_doctor_href_list[int(len(my_doctor_href_list)/5)*3:int(len(my_doctor_href_list)/5)*4]
-    my_doctor_href_list5 = my_doctor_href_list[int(len(my_doctor_href_list)/5)*4:len(my_doctor_href_list)]
-
-    # 执行多线程
-    t1 = MyThread(pathname=my_pathname, doctor_href_list=my_doctor_href_list1)
-    t2 = MyThread(pathname=my_pathname, doctor_href_list=my_doctor_href_list2)
-    t3 = MyThread(pathname=my_pathname, doctor_href_list=my_doctor_href_list3)
-    t4 = MyThread(pathname=my_pathname, doctor_href_list=my_doctor_href_list4)
-    t5 = MyThread(pathname=my_pathname, doctor_href_list=my_doctor_href_list5)
-
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t5.start()
-
-    t1.join()
-    t2.join()
-    t3.join()
-    t4.join()
-    t5.join()
+    # # 将医生分成5等份
+    # my_doctor_href_list1 = my_doctor_href_list[int(len(my_doctor_href_list)/5)*0:int(len(my_doctor_href_list)/5)*1]
+    # my_doctor_href_list2 = my_doctor_href_list[int(len(my_doctor_href_list)/5)*1:int(len(my_doctor_href_list)/5)*2]
+    # my_doctor_href_list3 = my_doctor_href_list[int(len(my_doctor_href_list)/5)*2:int(len(my_doctor_href_list)/5)*3]
+    # my_doctor_href_list4 = my_doctor_href_list[int(len(my_doctor_href_list)/5)*3:int(len(my_doctor_href_list)/5)*4]
+    # my_doctor_href_list5 = my_doctor_href_list[int(len(my_doctor_href_list)/5)*4:len(my_doctor_href_list)]
+    #
+    # # 执行多线程
+    # t1 = MyThread(pathname=my_pathname, doctor_href_list=my_doctor_href_list1)
+    # t2 = MyThread(pathname=my_pathname, doctor_href_list=my_doctor_href_list2)
+    # t3 = MyThread(pathname=my_pathname, doctor_href_list=my_doctor_href_list3)
+    # t4 = MyThread(pathname=my_pathname, doctor_href_list=my_doctor_href_list4)
+    # t5 = MyThread(pathname=my_pathname, doctor_href_list=my_doctor_href_list5)
+    #
+    # t1.start()
+    # t2.start()
+    # t3.start()
+    # t4.start()
+    # t5.start()
+    #
+    # t1.join()
+    # t2.join()
+    # t3.join()
+    # t4.join()
+    # t5.join()
